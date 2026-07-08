462c859afbbf203cc99a08dec53bf2de48bd1641
piadi-su <213212422+piadi-su@users.noreply.github.com>
2026-07-08 22:51:01 +0200

    first start config.h and main.c

---
+++ src/config.h
@@ -0,0 +1,36 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <X11/X.h>
+#include <X11/Xlib.h>
+#include <X11/keysym.h>
+
+// struct che definisce i KeyBinds
+typedef struct {
+	unsigned int mod;
+	KeySym keysym;
+	char **cmd;
+}KeyBinds;
+
+#define MODIFIER Mod4Mask
+
+//tutti i conandi
+static char *term_cmd[] = {"alacritty", NULL};
+static char *rofi_cmd[] = {"rofi", "-show", "drun", NULL};
+static char *close_wm[] = {"killall", "ash-wm", NULL};
+
+
+static KeyBinds keys[] = {
+// Modificatore | Tasto | Comando
+
+	{MODIFIER, XK_Return, term_cmd},
+	{MODIFIER, XK_d, rofi_cmd},
+	{MODIFIER, XK_u, close_wm},
+
+};
+
+// calcola il numero totale
+static const int num_keys = sizeof(keys) / sizeof(keys[0]);
+
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,141 @@
+#include <X11/X.h>
+#include <X11/Xlib.h>
+#include <X11/Xatom.h>
+#include <X11/XF86keysym.h>
+#include <X11/keysym.h>
+#include <X11/XKBlib.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h>
+
+//miei file
+#include "config.h"
+
+
+typedef struct Client {
+	Window id;
+	struct Client *next;
+	struct Client *prev;
+
+}Client;
+
+static Client *list_Cl = NULL;
+
+/*
+ * add a new window to the list 
+ * from the Ev Ev.xmaprequest.window
+ * */
+void 
+AddWindowList(Window w)
+{
+	//creo nuovo client e tolgo il garbage
+	Client *new_window = calloc(1, sizeof(Client));
+	if (new_window == NULL)
+		return;
+	
+	//aggiungimo la window nel id della strct 
+	new_window->id = w;
+
+		
+	//se la lista é nulla la inizializzo con il nuovo client
+	if(list_Cl == NULL)
+	{
+		/*
+		 prendo il pointer nullo lista_Cl
+		 e lo faccio puntare a new window 
+		 che contiene l'id della finestra
+		  */
+
+		list_Cl = new_window;
+		list_Cl->next = list_Cl;
+		list_Cl->prev = list_Cl;
+	}
+
+	else
+	{
+		new_window->next = list_Cl; // l'ultima finesta ha come next la prima
+		new_window->prev = list_Cl->prev; // l'ultima  ha come prev la penutlima
+		list_Cl->prev->next = new_window; // dicimao alla pen ultima che era il suo next é la ultima creata
+		list_Cl->next = new_window; // diciamo alla prima che come precendeta ha l'ultima
+	}
+
+
+	/*
+	 *  / c \
+	 *  b    d
+	 *  \ a /
+     *
+	 * */
+		
+
+
+}
+
+
+
+
+int main(void)
+{
+	
+	XEvent Ev;
+
+	Display *disp = XOpenDisplay(NULL);
+	if(disp == NULL){
+		fprintf(stderr, "error opening the display");
+		return 1;
+	}
+	
+	int sc = DefaultScreen(disp);
+
+	//create root window/desktop
+	Window root = RootWindow(disp, sc);
+
+
+	//serve ad un programma a registrarsi in coda 
+	XSelectInput(disp, root, SubstructureRedirectMask | SubstructureNotifyMask);
+
+	// svuota il buffer dove stanno le finestre in coda perche x11 e asincrono 
+	XSync(disp, False);
+
+
+	for(int i = 0 ; i < num_keys; i++)
+	{
+		// immagino prenda il codice di ogni keybinds nel array di struct KeYBinds
+		KeyCode code = XKeysymToKeycode(disp, keys[i].keysym);
+		XGrabKey(Display *, int, unsigned int, Window, int, int, int)
+	}
+
+	while(1)
+	{
+
+		XNextEvent(disp, &Ev);
+		
+		switch (Ev.type) {
+
+			case MapRequest:	 
+				AddWindowList(Ev.xmaprequest.window);
+
+				printf("[+] nuova finestra id: %lu\n", Ev.xmaprequest.window);
+
+				                    // questo é Window w
+				XMapWindow(disp, Ev.xmaprequest.window); //mappa/ fa vedere la finestra intercettata 
+				break;
+
+				
+
+			case DestroyNotify:	 // questo é Window w
+				break;
+
+
+			default:
+				break;
+		
+		}
+
+	}
+
+
+	XCloseDisplay(disp);
+	return 0;
+}
