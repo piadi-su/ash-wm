384822f283a5e88f472bcc19fa2388f44ee7a7f0
piadi-su <213212422+piadi-su@users.noreply.github.com>
2026-07-24 01:14:11 +0200

    rm useless comments

--- src/icccm.c
+++ src/icccm.c
@@ -11,23 +11,19 @@ void InitICCCM(Display *disp) {
     icccm.wm_change_state  = XInternAtom(disp, "WM_CHANGE_STATE", False);
 }
 
-// Applica le Size Hints (WM_NORMAL_HINTS) rispettando min/max size e resize increments
 void ApplyNormalHints(Display *disp, Window w, int *x, int *y, unsigned int *w_out, unsigned int *h_out) {
     XSizeHints hints;
     long supplied;
 
     if (XGetWMNormalHints(disp, w, &hints, &supplied)) {
-        // Gestione dimensioni minime
         if (hints.flags & PMinSize) {
             if (*w_out < (unsigned int)hints.min_width)  *w_out = hints.min_width;
             if (*h_out < (unsigned int)hints.min_height) *h_out = hints.min_height;
         }
-        // Gestione dimensioni massime
         if (hints.flags & PMaxSize) {
             if (hints.max_width > 0 && *w_out > (unsigned int)hints.max_width)   *w_out = hints.max_width;
             if (hints.max_height > 0 && *h_out > (unsigned int)hints.max_height) *h_out = hints.max_height;
         }
-        // Gestione incrementi di resize (utile per i terminali es. caratteri 8x16)
         if (hints.flags & PResizeInc) {
             if (hints.width_inc > 0)  *w_out -= (*w_out - (hints.flags & PBaseSize ? hints.base_width : 0)) % hints.width_inc;
             if (hints.height_inc > 0) *h_out -= (*h_out - (hints.flags & PBaseSize ? hints.base_height : 0)) % hints.height_inc;
